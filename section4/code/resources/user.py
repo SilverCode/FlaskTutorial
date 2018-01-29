73b967b3ec05ad6e9b6c1d4bd8103cf9086af050
Ross Simpson <ross@onedigitalmedia.com>
2018-01-29 16:03:16 +0200

    Rename User class to UserModel

--- section4/code/resources/user.py
+++ section4/code/resources/user.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask_restful import Resource, reqparse
+from models.user import UserModel
 
 class UserRegister(Resource):
 
@@ -10,7 +11,7 @@ class UserRegister(Resource):
     def post(self):
         data = UserRegister.parser.parse_args()
 
-        if User.find_by_username(data['username']):
+        if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
 
         connection = sqlite3.connect('data.db')
